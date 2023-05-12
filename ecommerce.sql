73061c28114fd807cf58dcca16a8569eb19c9e93
matteo2321 <matteoscuola21@gmail.com>
2023-05-12 11:25:07 +0200

    consegna

--- ecommerce.sql
+++ ecommerce.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 11, 2023 alle 23:38
--- Versione del server: 10.4.25-MariaDB
--- Versione PHP: 8.1.10
+-- Creato il: Mag 12, 2023 alle 11:18
+-- Versione del server: 10.4.28-MariaDB
+-- Versione PHP: 8.1.17
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -31,14 +31,14 @@ CREATE TABLE `carrello` (
   `id` int(32) NOT NULL,
   `data` date NOT NULL,
   `IdUtente` int(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dump dei dati per la tabella `carrello`
 --
 
 INSERT INTO `carrello` (`id`, `data`, `IdUtente`) VALUES
-(2, '2023-05-10', 1);
+(1, '2023-05-11', 1);
 
 -- --------------------------------------------------------
 
@@ -49,7 +49,7 @@ INSERT INTO `carrello` (`id`, `data`, `IdUtente`) VALUES
 CREATE TABLE `categoria` (
   `idc` int(32) NOT NULL,
   `nome` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dump dei dati per la tabella `categoria`
@@ -67,21 +67,22 @@ INSERT INTO `categoria` (`idc`, `nome`) VALUES
 --
 
 CREATE TABLE `commento` (
-  `id` int(32) NOT NULL,
+  `idcom` int(32) NOT NULL,
   `text` varchar(256) NOT NULL,
   `stelle` int(1) NOT NULL,
   `IdUtente` int(32) NOT NULL,
   `IdProdotto` int(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dump dei dati per la tabella `commento`
 --
 
-INSERT INTO `commento` (`id`, `text`, `stelle`, `IdUtente`, `IdProdotto`) VALUES
-(7, 'ottimo prodotto', 4, 1, 2),
-(9, 'ottimo prodotto', 1, 1, 3),
-(10, 'ottimo prodotto', 5, 1, 3);
+INSERT INTO `commento` (`idcom`, `text`, `stelle`, `IdUtente`, `IdProdotto`) VALUES
+(1, 'ottimo prodotto', 1, 1, 3),
+(2, 'gg', 1, 1, 3),
+(3, 'gg', 3, 1, 3),
+(4, 'ciao', 5, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -94,14 +95,14 @@ CREATE TABLE `contiene` (
   `IdCarrello` int(32) NOT NULL,
   `IdProdotto` int(32) NOT NULL,
   `quantita` int(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dump dei dati per la tabella `contiene`
 --
 
 INSERT INTO `contiene` (`id`, `IdCarrello`, `IdProdotto`, `quantita`) VALUES
-(5, 2, 3, 3);
+(1, 1, 2, 3);
 
 -- --------------------------------------------------------
 
@@ -112,7 +113,7 @@ INSERT INTO `contiene` (`id`, `IdCarrello`, `IdProdotto`, `quantita`) VALUES
 CREATE TABLE `foto` (
   `idf` int(32) NOT NULL,
   `path` varchar(256) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dump dei dati per la tabella `foto`
@@ -135,7 +136,7 @@ CREATE TABLE `ordine` (
   `data` date NOT NULL,
   `prezzo` int(32) NOT NULL,
   `IdCarrello` int(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -148,19 +149,19 @@ CREATE TABLE `prodotto` (
   `titolo` varchar(256) NOT NULL,
   `descrizione` text NOT NULL,
   `venditore` varchar(256) NOT NULL,
-  `quantitaMag` varchar(256) NOT NULL,
+  `quantita` varchar(256) NOT NULL,
   `prezzo` int(32) NOT NULL,
   `IdCategoria` int(32) NOT NULL,
   `IdFoto` int(32) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dump dei dati per la tabella `prodotto`
 --
 
-INSERT INTO `prodotto` (`id`, `titolo`, `descrizione`, `venditore`, `quantitaMag`, `prezzo`, `IdCategoria`, `IdFoto`) VALUES
+INSERT INTO `prodotto` (`id`, `titolo`, `descrizione`, `venditore`, `quantita`, `prezzo`, `IdCategoria`, `IdFoto`) VALUES
 (2, 'ThinkBook 16 Gen 4', 'Il notebook ThinkBook 16 di quarta generazione offre ancora di più. Maggiore potenza, grazie al processore Intel® Core™ di dodicesima generazione. Anche maggiore spazio sullo schermo: quello da 40,64 cm (16\") è il più grande della linea ThinkBook, con una risoluzione fino a 2,5K e opzioni di grafica avanzata. La connettività wireless è stata aggiornata rispetto alle versioni precedenti e nuove funzionalità e nuovi sensori smart ti aiutano a garantire la produttività e a proteggere i tuoi dati aziendali.', 'Lenovo', '30', 1039, 1, 1),
-(3, 'Apple iPhone 14 (128 GB)eheheheheh', 'Display Super Retina XDR da 6,1\"\r\nUna batteria che dura tutto il giorno e ti dà fino a 20 ore di riproduzione video\r\nCeramic Shield e resistenza all’acqua per una robustezza all’avanguardia nel settore\r\nChip A15 Bionic con GPU 5-core per prestazioni fulminee. Reti cellulari 5G ultrarapide\r\nSistema di fotocamere evoluto per scatti più belli con ogni tipo di luce\r\nModalità Azione per riprese stabili e senza sbalzi\r\nModalità Cinema: ora con Dolby Vision 4K fino a 30 fps\r\nFunzioni di sicurezza: SOS emergenze via satellite e Rilevamento incidenti\r\niOS 16: ancora più modi per comunicare e condividere, e per rendere il tuo iPhone sempre più tuo', 'Apple', '10', 829, 2, 2),
+(3, 'Apple iPhone 14 (128 GB)', 'Display Super Retina XDR da 6,1\"\r\nUna batteria che dura tutto il giorno e ti dà fino a 20 ore di riproduzione video\r\nCeramic Shield e resistenza all’acqua per una robustezza all’avanguardia nel settore\r\nChip A15 Bionic con GPU 5-core per prestazioni fulminee. Reti cellulari 5G ultrarapide\r\nSistema di fotocamere evoluto per scatti più belli con ogni tipo di luce\r\nModalità Azione per riprese stabili e senza sbalzi\r\nModalità Cinema: ora con Dolby Vision 4K fino a 30 fps\r\nFunzioni di sicurezza: SOS emergenze via satellite e Rilevamento incidenti\r\niOS 16: ancora più modi per comunicare e condividere, e per rendere il tuo iPhone sempre più tuo', 'Apple', '10', 829, 2, 2),
 (4, 'Logitech G203 LIGHTSYNC', 'Sensore da 8; 000 DPI che risponde con precisione a tutti i movimenti: personalizza le impostazioni in base alla sensibilità che desideri con il software per gaming logitech g hub e scegli fra 5 livelli DPI passando facilmente dall\'uno all\'altro\r\nColori LIGHTSYNC RGB: goditi colori vivi e brillanti grazie al nostro sistema LIGHTSYNC RGB, che offre effetti personalizzabili su ~16,8 milioni di colori; installa il software logitech g hub per scegliere tra i colori e le animazioni preimpostati o creare i tuoi\r\nDesign classico testato dai giocatori: gioca in tutto comfort e con controllo totale; l\'intuitivo layout a 6 pulsanti e la classica forma da gaming definiscono un design confortevole, collaudato nel tempo e amato dai giocatori, ciascun pulsante può essere personalizzato con il software logitech g hub per semplificare le operazioni\r\nTensionamento meccanico dei pulsanti con molla in metallo: I pulsanti primari sono meccanici e presentano tensionamento con resistenti molle in metallo per garantire affidabilità, prestazioni e maneggevolezza ottime\r\nIl Brand di Accessori Gaming più venduto al mondo: sulla base dei dati di vendita aggregati indipendenti (FEB \'19 - FEB\'20) di tastiere, mouse, cuffie gaming per PC in unità', 'Logitech', '5', 25, 3, 3),
 (5, 'Logitech MX Keys', 'Fluida, Naturale, Precisa : Digitazione perfetta PerfectStroke, tasti concavi si adattano ‎alla forma delle dita e i bordi arrotondati dal feedback soddisfacente ovunque vengono toccati\r\nComfort e Stabilità : Digita con sicurezza sulla tastiera piatta e sottile per ‎stabilità, precisione e resistenza. La stabilità riduce il rumore e sentirai, senza però udire, ‎ogni pressione di tasto\r\nIlluminazione Intelligente, Ancora Più Smart: I tasti si illuminano nel momento in ‎cui le mani si avvicinano. La retroilluminazione si regola in automatico adattandosi alle ‎condizioni di luce\r\nLa tastiera wireless MX Keys Segue il mouse con il supporto Flow da un computer ‎all\'altro, digita su più dispositivi in un solo flusso di lavoro. Trasferisci file e immagini su ‎computer Mac/Windows\r\nTastiera Multi-Device & Multi-OS: Connetti con Bluetooth o Unifying ‎USB 3 dispositivi e passa da uno all\'altro con la pressione di un pulsante. ‎Compatibile con Windows, macOS, Linux, iOS, Android\r\nRicaricabile Tramite USB-C: MX Keys funziona fino a 10 giorni con una ricarica completa ‎‎– o fino a 5 mesi con retroilluminazione disattivata.\r\nProva la Tastiera Logitech CRAFT con Pulsante Girevole: Per il controllo creativo e accesso a ‎funzioni specifiche per contesto. Personalizza i profili per le tue app preferite', 'Logitech', '25', 89, 3, 4);
 
@@ -178,15 +179,14 @@ CREATE TABLE `utente` (
   `password` varchar(256) NOT NULL,
   `indirizzo` varchar(256) NOT NULL,
   `admin` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dump dei dati per la tabella `utente`
 --
 
 INSERT INTO `utente` (`id`, `nome`, `cognome`, `email`, `password`, `indirizzo`, `admin`) VALUES
-(1, 'admin', 'admin', 'admin@admin.com', 'password', 'admin', 1),
-(3, 'matteo', 'crippa', '', 'Bassotto23', 'via Spinola 7, Cantù CO, 22063', 0);
+(1, 'admin', 'admin', 'admin@email.com', 'password', 'admin1', 1);
 
 --
 -- Indici per le tabelle scaricate
@@ -209,7 +209,7 @@ ALTER TABLE `categoria`
 -- Indici per le tabelle `commento`
 --
 ALTER TABLE `commento`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`idcom`),
   ADD KEY `FKUtente2` (`IdUtente`),
   ADD KEY `FKProdotto` (`IdProdotto`);
 
@@ -256,25 +256,25 @@ ALTER TABLE `utente`
 -- AUTO_INCREMENT per la tabella `carrello`
 --
 ALTER TABLE `carrello`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT per la tabella `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `idc` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idc` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 
 --
 -- AUTO_INCREMENT per la tabella `commento`
 --
 ALTER TABLE `commento`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `idcom` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT per la tabella `contiene`
 --
 ALTER TABLE `contiene`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT per la tabella `foto`
@@ -298,7 +298,7 @@ ALTER TABLE `prodotto`
 -- AUTO_INCREMENT per la tabella `utente`
 --
 ALTER TABLE `utente`
-  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(32) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- Limiti per le tabelle scaricate
